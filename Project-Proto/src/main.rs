9df64723c5a442213fac05a26cbf6490c383977b
pii1k <ajy0535@naver.com>
2025-09-04 19:57:37 +0900

    init

--- Project-Proto/src/main.rs
+++ Project-Proto/src/main.rs
@@ -1,3 +1,22 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "Project: Proto".into(),
+                    resolution: (600.0, 800.0).into(),
+                    resizable: false,
+                    ..default()
+                }),
+                ..default()
+            }),
+        )
+        .add_systems(Startup, setup_camera)
+        .run();
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
 }
