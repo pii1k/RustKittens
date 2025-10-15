9e78d3e3411151a6c0b0278d5bb930da816c8ac7
TonyLee <tonylee221b@naver.com>
2025-10-16 00:47:39 +0900

    External 리팩토링

--- Project-Proto/src/external/mod.rs
+++ Project-Proto/src/external/mod.rs
@@ -1,7 +1,9 @@
 use bevy::prelude::*;
+use bevy_egui::EguiPlugin;
+use ui::inspector::EditorPlugin;
 
 mod ui;
 
 pub fn plugin(app: &mut App) {
-    app.add_plugins(ui::editor::components::EditorPlugin);
+    app.add_plugins((EguiPlugin, EditorPlugin));
 }
