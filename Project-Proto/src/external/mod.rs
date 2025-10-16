a2a3c41393348674afb791738b144ccdc8f9c478
Hyeongseok Lee <tonyhslee@autocrypt.io>
2025-10-16 19:59:49 +0900

    ui -> debug 바꿈 ㅅㄱ

--- Project-Proto/src/external/mod.rs
+++ Project-Proto/src/external/mod.rs
@@ -1,8 +1,8 @@
 use bevy::prelude::*;
 use bevy_egui::EguiPlugin;
-use ui::inspector::EditorPlugin;
+use debug::inspector::EditorPlugin;
 
-mod ui;
+mod debug;
 
 pub fn plugin(app: &mut App) {
     app.add_plugins((EguiPlugin, EditorPlugin));
